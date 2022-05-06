e6985982655ffed85eccc46c1387fd03d5f5f77c
wesleymaicon <wesleymaicon002@gmail.com>
2022-05-06 16:56:26 -0300

    Ajust tests and readFile function to support '\r\n' and '\n' line breaks

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -6,7 +6,7 @@ export const readFile = async (fileName: string): Promise<string[]>  => {
     fs.createReadStream(fileName, {encoding: 'utf-8'})
     .on("error", err => reject(err))
     .on('data', (data) => {
-      // Remove square brackets, replace \r\n to \n and create array
+      // Remove square brackets, replace \r\n to \n and create arrayte
       fetchData = data.toString().replace(/\[/g, '').replace(/]/g, '').split('\r\n').join('\n').split('\n');
       // If the last item is empty, remove it from array
       if ((fetchData.length > 0) && (fetchData[fetchData.length - 1].trim() === '')) {
